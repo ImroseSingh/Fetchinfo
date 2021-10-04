31f7fb1e7b0c7a30616d43610e13e5acca2d289b
ImroseSingh <90102995+ImroseSingh@users.noreply.github.com>
2021-10-03 20:06:57 -0400

    Update hw04.py

--- hw04.py
+++ hw04.py
@@ -6,13 +6,13 @@ from Getinfo import GitHubAPI
 class TestGitHubAPI(unittest.TestCase):
 
     def test_myrepo(self):
-        gh = GitHubAPI("ImroseSingh")
-        self.assertEqual(len(gh.get_repos()), 4,
+        fi = GitHubAPI("ImroseSingh")
+        self.assertEqual(len(fi.get_repos()), 4,
                          "There are 4 repositores")
 
     def test_Getrepo(self):
-        gh = GitHubAPI("ImroseSingh")
-        self.assertEqual(gh.get_repos(), ['Fetchinfo', 'ImroseSingh', 'ssw-567', 'triangle-HW2'], [
+        fi = GitHubAPI("ImroseSingh")
+        self.assertEqual(fi.get_repos(), ['Fetchinfo', 'ImroseSingh', 'ssw-567', 'triangle-HW2'], [
                          'Fetchinfo', 'ImroseSingh', 'ssw-567', 'triangle-HW2'])
 
 
