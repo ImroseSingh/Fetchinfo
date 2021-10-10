0f826efdb919a385e47d2058dd06d977d0385cb7
ImroseSingh <90102995+ImroseSingh@users.noreply.github.com>
2021-10-10 17:11:45 -0400

    Update hw04.py

--- hw04.py
+++ hw04.py
@@ -1,10 +1,11 @@
 import unittest
 import sys
+import unittest.mock import Mock, patch
 
 from Getinfo import GitHubAPI
 
 class TestGitHubAPI(unittest.TestCase):
-
+    
     def test_myrepo(self):
         fi = GitHubAPI("ImroseSingh")
         self.assertEqual(len(fi.get_repos()), 4,
@@ -14,6 +15,11 @@ class TestGitHubAPI(unittest.TestCase):
         fi = GitHubAPI("ImroseSingh")
         self.assertEqual(fi.get_repos(), ['Fetchinfo', 'ImroseSingh', 'ssw-567', 'triangle-HW2'], [
                          'Fetchinfo', 'ImroseSingh', 'ssw-567', 'triangle-HW2'])
+     @patch('github_api.requests.get')
+    def test_return_not_ok(self, mock_get):
+        mock_get.return_value.ok = False
+        response = list_repositories('testUser')
+        self.assertEqual("Invalid response from API", response)
 
 
   
