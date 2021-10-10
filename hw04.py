c0d8f6e206f3fb60e297ed657d8cbfcfe527db46
ImroseSingh <90102995+ImroseSingh@users.noreply.github.com>
2021-10-10 17:16:59 -0400

    Update hw04.py

--- hw04.py
+++ hw04.py
@@ -17,10 +17,22 @@ class TestGitHubAPI(unittest.TestCase):
         self.assertEqual(fi.get_repos(), ['Fetchinfo', 'ImroseSingh', 'ssw-567', 'triangle-HW2'], [
                          'Fetchinfo', 'ImroseSingh', 'ssw-567', 'triangle-HW2'])
      @patch('github_api.requests.get')
-    def test_return_not_ok(self, mock_get):
-        mock_get.return_value.ok = False
+    def test_correct_output(self, mock_get):
+        repo_list = [
+            {'name': "test_repo1"},
+            {'name': "test_repo2"}
+        ]
+        commit_list_1 = [
+            {'node_id': 'NDKSJHMDKNSUHDH'},
+            {'node_id': 'kshdahoedaenodeno'}
+        ]
+        commit_list_2 = [
+            {'node_id': 'NDKSJHMDKNSUHDH'}
+        ]
+        mock_get.return_value.ok = True
+        mock_get.return_value.json.side_effect = [repo_list, commit_list_1, commit_list_2]
         response = list_repositories('testUser')
-        self.assertEqual("Invalid response from API", response)
+        self.assertEqual([['test_repo1', 2], ['test_repo2', 1]], response)
 
 
   
