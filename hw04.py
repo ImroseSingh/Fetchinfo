d31c9235330a05f6d702f50a64c4493899079b11
ImroseSingh <90102995+ImroseSingh@users.noreply.github.com>
2021-10-11 09:15:53 -0400

    Update hw04.py

--- hw04.py
+++ hw04.py
@@ -3,13 +3,11 @@ import sys
 import json
 from unittest.mock import MagicMock as Mock
 from unittest.mock import patch
-
 from Getinfo import GitHubAPI
 
 class TestGitHubAPI(unittest.TestCase):
-    
-    
-     def test_myrepo(self):
+
+    def test_myrepo(self):
         fi = GitHubAPI("ImroseSingh")
         self.assertEqual(len(fi.get_repos()), 4,
                          "There are 4 repositores")
@@ -18,7 +16,6 @@ class TestGitHubAPI(unittest.TestCase):
         fi = GitHubAPI("ImroseSingh")
         self.assertEqual(fi.get_repos(), ['Fetchinfo', 'ImroseSingh', 'ssw-567', 'triangle-HW2'], [
                          'Fetchinfo', 'ImroseSingh', 'ssw-567', 'triangle-HW2'])
-    
 
 
   
@@ -26,8 +23,6 @@ class TestGitHubAPI(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestGetRepo)
-    unittest.TextTestRunner(verbosity = 2).run(suite)
 
    
 
