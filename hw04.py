8f483c8d0d61aff750664dbdb84250d0109abafb
ImroseSingh <90102995+ImroseSingh@users.noreply.github.com>
2021-10-11 08:29:15 -0400

    Update hw04.py

--- hw04.py
+++ hw04.py
@@ -1,38 +1,26 @@
 import unittest
-import sys
-import unittest.mock 
-import Mock, patch
+import json
+import get_repo
+from unittest.mock import MagicMock as Mock
+from unittest.mock import patch
 
 from Getinfo import GitHubAPI
 
 class TestGitHubAPI(unittest.TestCase):
     
-    def test_myrepo(self):
+    def testConnection(self, mock_connect):
         fi = GitHubAPI("ImroseSingh")
         self.assertEqual(len(fi.get_repos()), 4,
                          "There are 4 repositores")
-
-    def test_Getrepo(self):
-        fi = GitHubAPI("ImroseSingh")
-        self.assertEqual(fi.get_repos(), ['Fetchinfo', 'ImroseSingh', 'ssw-567', 'triangle-HW2'], [
-                         'Fetchinfo', 'ImroseSingh', 'ssw-567', 'triangle-HW2'])
-     @patch('github_api.requests.get')
-    def test_correct_output(self, mock_get):
-        repo_list = [
-            {'name': "test_repo1"},
-            {'name': "test_repo2"}
-        ]
-        commit_list_1 = [
-            {'node_id': 'NDKSJHMDKNSUHDH'},
-            {'node_id': 'kshdahoedaenodeno'}
-        ]
-        commit_list_2 = [
-            {'node_id': 'NDKSJHMDKNSUHDH'}
-        ]
-        mock_get.return_value.ok = True
-        mock_get.return_value.json.side_effect = [repo_list, commit_list_1, commit_list_2]
-        response = list_repositories('testUser')
-        self.assertEqual([['test_repo1', 2], ['test_repo2', 1]], response)
+    @patch("get_repo.connect")
+    def testConnection(self, mock_connect):
+        mock_connect.return_value = [200, {"CS-523-Web-Programming-1": "1",
+                                     "CS555---GEDCOM-Project": "13",
+                                     "GitHubAPI567": "12",
+                                     "HelloWorldRepo": "3",
+                                     "Triangle567": "8"}]
+        self.assertTrue(get_repo.connect("cmontero201")[0])
+    
 
 
   
@@ -40,6 +28,8 @@ class TestGitHubAPI(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+     suite = unittest.TestLoader().loadTestsFromTestCase(TestGetRepo)
+    unittest.TextTestRunner(verbosity = 2).run(suite)
 
    
 
