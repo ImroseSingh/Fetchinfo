0ed336820bc4c274ff823ea98a3cc35cf7d0806e
ImroseSingh <90102995+ImroseSingh@users.noreply.github.com>
2021-10-16 15:15:26 -0400

    Update Getinfo.py

--- Getinfo.py
+++ Getinfo.py
@@ -1,36 +1,41 @@
 import requests
 import json
 
- class GitHubAPI:
-
-     def __init__(user, id):
-         user.id = id      
-        
-
-         user.repos = []
-         user.commits = {}
-
-     def get_repos(user):
-         try:
-             r = requests.get(f'https://api.github.com/users/{user.id}/repos')
-
-         except requests.exceptions as error:
-             return
-         for repo in r.json():
-             user.repos.append(repo["name"])
-         return user.repos
-
-     def get_commits(user):
-         for repo in user.repos:
-             try:
-                 r = requests.get(f'https://api.github.com/repos/{user.id}/{repo}/commits')
-             except requests.exceptions as error:
-                 return
-             user.commits[repo] = len(r.json())
-         return user.commits
-
+class GitHubAPI:
     
+    def __init__(self, id):
+        self.id = id      
+        self.repo_status = 0
+        self.commit_status = 0
 
- if __name__ == "__main__":
-     githubapi = GitHubAPI("ImroseSingh")
+        self.repos = []
+        self.commits = {}
+
+    def get_repos(self):
+        try:
+            r = requests.get(f'https://api.github.com/users/{self.id}/repos')
+            
+        except requests.exceptions.HTTPError as error:
+            return
+        for repo in r.json():
+            self.repos.append(repo["name"])
+        return self.repos
+
+    def get_commits(self):
+        for repo in self.repos:
+            try:
+                r = requests.get(f'https://api.github.com/repos/{self.id}/{repo}/commits')
+            except requests.exceptions.HTTPError as error:
+                return
+            self.commits[repo] = len(r.json())
+        return self.commits
+
+    def print_data(self):
+        for repo in self.repos:
+            print(f'Repo: {repo} Number of commits: {self.commits[repo]}')
+
+   
+
+if __name__ == "__main__":
+    githubapi = GitHubAPI("ImroseSingh")
    
