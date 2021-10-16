362e5c1fc393b1f3aa1c97b162e065d8beb84db7
ImroseSingh <90102995+ImroseSingh@users.noreply.github.com>
2021-10-16 15:11:06 -0400

    Update Getinfo.py

--- Getinfo.py
+++ Getinfo.py
@@ -14,7 +14,7 @@ import json
          try:
              r = requests.get(f'https://api.github.com/users/{user.id}/repos')
 
-         except requests.exceptions.HTTPError as error:
+         except requests.exceptions as error:
              return
          for repo in r.json():
              user.repos.append(repo["name"])
@@ -24,7 +24,7 @@ import json
          for repo in user.repos:
              try:
                  r = requests.get(f'https://api.github.com/repos/{user.id}/{repo}/commits')
-             except requests.exceptions.HTTPError as error:
+             except requests.exceptions as error:
                  return
              user.commits[repo] = len(r.json())
          return user.commits
